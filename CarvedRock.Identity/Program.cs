37023e0b223b0915e6f71bf9391d741e1d1f5950
Stuart Murray <stuart.murray.dev@outlook.com>
2025-05-13 17:53:57 +0100

    debug identity

--- CarvedRock.Identity/Program.cs
+++ CarvedRock.Identity/Program.cs
@@ -1,4 +1,5 @@
-﻿using CarvedRock.Identity;
+﻿using System.Net;
+using CarvedRock.Identity;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -11,6 +12,18 @@ try
 {
     var builder = WebApplication.CreateBuilder(args);
 
+    var useCustomLocalCert = builder.Configuration.GetValue<bool>("UseCustomLocalCert");
+    if (useCustomLocalCert)
+    {
+        builder.WebHost.ConfigureKestrel((context, options) =>
+        {
+            options.Listen(IPAddress.Any, 8091, listenOptions =>
+            {
+                listenOptions.UseHttps("keys/cr-id-local.pfx", "Learning1sGreat!");
+            });
+        });
+    }
+
     builder.Host.UseSerilog((context, lc) => lc
         .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
         .WriteTo.Seq(context.Configuration.GetValue<string>("SeqAddress")!)
@@ -20,7 +33,7 @@ try
     var app = builder
         .ConfigureServices()
         .ConfigurePipeline();
-    
+
     app.Run();
 }
 catch (Exception ex)
